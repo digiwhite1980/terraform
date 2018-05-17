7a02b69f17bb4949dafa5a564b7ae3de0540a3c2
Raymond Otto <otto@rgpo.nl>
2018-05-17 10:30:50 +0200

    Launch configuration user_data => user_base_base64

--- launch_configuration/launch_configuration.tf
+++ launch_configuration/launch_configuration.tf
@@ -6,7 +6,7 @@ resource "aws_launch_configuration" "launch_configuration" {
   spot_price           = "${var.spot_price}"
   iam_instance_profile = "${var.iam_instance_profile}"
 
-  user_data             = "${var.user_data}"
+  user_data_base64      = "${var.user_data}"
   key_name              = "${var.key_name}"
 
   security_groups       = [ "${var.security_groups}" ]
