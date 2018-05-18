bbd50ee4113f568ede8bc123ceeb641561525cc1
Raymond Otto <otto@rgpo.nl>
2018-05-18 08:14:05 +0200

    disable id output for launhconfiguration

--- launch_configuration/output.tf
+++ launch_configuration/output.tf
@@ -1,6 +1,6 @@
-output "id" {
-	value = "${aws_launch_configuration.launch_configuration.id}"
-}
+#output "id" {
+#	value = "${aws_launch_configuration.launch_configuration.id}"
+#}
 
 output "name" {
   value = "${aws_launch_configuration.launch_configuration.name}"
