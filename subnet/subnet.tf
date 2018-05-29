2b4d6036c8fbaf9cdc64d136c73f81a4285876e7
Raymond Otto <otto@rgpo.nl>
2018-05-29 18:22:23 +0200

    env added

--- subnet/subnet.tf
+++ subnet/subnet.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "subnet" {
     vpc_id 					= "${var.vpc_id}"
-    cidr_block              = "${lookup(var.cidr_block, "avz.${element(var.availability_zone, count.index)}")}"
+    cidr_block              = "${lookup(var.cidr_block, "avz.${count.index}")}"
     availability_zone		= "${element(var.availability_zone, count.index)}"
     count                   = "${length(var.availability_zone)}"
 
