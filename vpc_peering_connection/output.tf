015fc39be3f0a9ea44681a6f98be77c2acb8f39e
Raymond Otto <otto@rgpo.nl>
2018-12-04 15:57:33 +0100

    Peer id output

--- vpc_peering_connection/output.tf
+++ vpc_peering_connection/output.tf
@@ -1,3 +1,7 @@
+output "id" {
+        value = "${aws_vpc_peering_connection.vpc_peering.id}"
+}
+
 output "accept_status" {
         value = "${aws_vpc_peering_connection.vpc_peering.accept_status}"
 }
