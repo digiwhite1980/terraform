95196e457692dc565b0be96b65fb91ef81f86e19
Raymond Otto <otto@rgpo.nl>
2018-12-04 15:54:00 +0100

    added route for peering

---
+++ route_table_peering/route_table.tf
@@ -0,0 +1,12 @@
+resource "aws_route_table" "route_peering" {
+    vpc_id 			= "${var.vpc_id}"
+    route {
+        cidr_block 						= "${var.cidr_block}"
+        vpc_peering_connection_id 	= "${var.vpc_peering_connection_id}"
+    }
+
+    tags {
+        Name 		= "${var.name} ${var.project} ${var.environment}"
+        Environment	= "${var.environment}"
+    }
+}
