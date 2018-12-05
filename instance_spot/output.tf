cfefb39a37afe6a3fe8098a3fe1181b232146d1c
Raymond Otto <otto@rgpo.nl>
2018-12-05 11:00:22 +0100

    Removed output for spot instances

--- instance_spot/output.tf
+++ instance_spot/output.tf
@@ -2,30 +2,6 @@ output "instance_name" {
 	value = "${var.instance_name}"
 }
 
-output "public_ip" {
-  value = [
-    "${aws_spot_instance_request.instance.*.public_ip}"
-  ]
-}
-
-output "private_ip" {
-  value = [
-    "${aws_spot_instance_request.instance.*.private_ip}"
-  ]
-}
-
-output "public_dns" {
-  value = [
-    "${aws_spot_instance_request.instance.*.public_dns}"
-  ]
-}
-
-output "private_dns" {
-  value = [
-    "${aws_spot_instance_request.instance.*.private_dns}"
-  ]
-}
-
 output "id" {
   value = [
     "${aws_spot_instance_request.instance.*.id}"
