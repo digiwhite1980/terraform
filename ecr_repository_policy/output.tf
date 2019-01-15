b003e5097c284cae0f85d9308f8c844446a5e3ed
Raymond Otto <otto@rgpo.nl>
2019-01-15 16:38:53 +0100

    Typo in output module

--- ecr_repository_policy/output.tf
+++ ecr_repository_policy/output.tf
@@ -1,7 +1,7 @@
 output "repository" {
-	value = "${aws_ecr_lifecycle_policy.policy.repository}"
+	value = "${aws_ecr_repository_policy.policy.repository}"
 }
 
 output "registry_id" {
-	value = "${aws_ecr_lifecycle_policy.policy.registry_id}"
+	value = "${aws_ecr_repository_policy.policy.registry_id}"
 }
