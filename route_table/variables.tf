c3a5ade7daa650dc0630253381deb747843d283c
Raymond Otto <otto@rgpo.nl>
2019-02-13 09:20:37 +0100

    Make roue dynamic

--- route_table/variables.tf
+++ route_table/variables.tf
@@ -1,6 +1,10 @@
 variable "vpc_id"		   {}
-variable "cidr_block"		{}
-variable "gateway_id"		{}
+#variable "cidr_block"		{}
+#variable "gateway_id"		{}
+
+variable "route"				{
+	type = "list"
+}
 
 variable "name" 		{
 	default = "Route_table"
