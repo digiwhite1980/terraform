d0717a7ada3c6d9a12b2c26c2ed64f830b236f4d
Raymond Otto <otto@rgpo.nl>
2019-02-13 09:23:42 +0100

    Make roue dynamic

--- route_table/route_table.tf
+++ route_table/route_table.tf
@@ -1,9 +1,7 @@
 resource "aws_route_table" "route" {
     vpc_id 			= "${var.vpc_id}"
-    route {
-        cidr_block 	= "${var.cidr_block}"
-        gateway_id 	= "${var.gateway_id}"
-    }
+
+    route       = "${var.route}"
 
     tags {
         Name 		= "${var.name} ${var.project} ${var.environment}"
