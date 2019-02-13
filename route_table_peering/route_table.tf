6fb14b96e49ebd25bc44855cea0697b05845c652
Raymond Otto <otto@rgpo.nl>
2019-02-13 11:19:53 +0100

    Routing for peer

--- route_table_peering/route_table.tf
+++ route_table_peering/route_table.tf
@@ -1,12 +1,14 @@
 resource "aws_route_table" "route" {
-    vpc_id 			= "${var.vpc_id}"
+    vpc_id 			        = "${var.vpc_id}"
+    count               = "${var.count}"
+
     route {
-        cidr_block 						= "${var.cidr_block}"
-        vpc_peering_connection_id 	= "${var.vpc_peering_connection_id}"
+        cidr_block                = "${var.cidr_block}"
+        vpc_peering_connection_id = "${element(var.gateway_id, count.index)}"
     }
 
     tags {
-        Name 		= "${var.name} ${var.project} ${var.environment}"
-        Environment	= "${var.environment}"
+        Name        = "${var.name} ${element(var.gateway_id, count.index)} ${var.project} ${var.environment}"
+        Environment     = "${var.environment}"
     }
 }
--- route_table_peering/variables.tf
+++ route_table_peering/variables.tf
@@ -1,6 +1,9 @@
 variable "vpc_id"		   {}
 variable "cidr_block"		{}
 variable "vpc_peering_connection_id"		{}
+variable "count" {
+  default = 1
+}
 
 variable "name" 		{
 	default = "Route_table"
