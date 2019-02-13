10231a40d21bcca4dc54efcb29232093765daffa
Raymond Otto <otto@rgpo.nl>
2019-02-13 13:06:41 +0100

    routing

--- route_table/route_table.tf
+++ route_table/route_table.tf
@@ -1,10 +1,10 @@
 resource "aws_route_table" "route" {
     vpc_id 			= "${var.vpc_id}"
 
-    route {
-        cidr_block      = "${var.cidr_block}"
-        nat_gateway_id  = "${var.gateway_id}"
-    }
+    # route {
+    #     cidr_block      = "${var.cidr_block}"
+    #     nat_gateway_id  = "${var.gateway_id}"
+    # }
 
     tags {
         Name 		= "${var.name} ${var.project} ${var.environment}"
--- route_table/variables.tf
+++ route_table/variables.tf
@@ -1,6 +1,6 @@
 variable "vpc_id"		   {}
-variable "cidr_block"		{}
-variable "gateway_id"		{}
+# variable "cidr_block"		{}
+# variable "gateway_id"		{}
 
 variable "name" 		{
 	default = "Route_table"
