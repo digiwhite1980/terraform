1ac4c21bd8900ee412c5985e84352aef8a47f114
Raymond Otto <otto@rgpo.nl>
2019-02-13 11:38:58 +0100

    route peering

--- route_peering/route_peering.tf
+++ route_peering/route_peering.tf
@@ -1,5 +1,8 @@
 resource "aws_route" "route" {
-	route_table_id					= "${var.route_table_id}"
-	destination_cidr_block		= "${var.destination_cidr_block}"
+  count                   = "${length(var.route_table_id)}"
+
+	route_table_id					= "${element(var.route_table_id, count.index)}"
+	
+  destination_cidr_block		= "${var.destination_cidr_block}"
 	vpc_peering_connection_id	= "${var.vpc_peering_connection_id}"
 }
