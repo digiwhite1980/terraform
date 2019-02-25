d1f15d477aebac34d1090590701424e0fe108ee8
Raymond Otto <otto@rgpo.nl>
2019-02-25 11:47:29 +0100

    tabs added

--- launch_configuration/launch_configuration.tf
+++ launch_configuration/launch_configuration.tf
@@ -2,31 +2,31 @@ resource "aws_launch_configuration" "launch_configuration" {
 	name_prefix			     = "${var.name_prefix}"
 
 	image_id			       = "${var.image_id}"
-  instance_type        = "${var.instance_type}"
-  spot_price           = "${var.spot_price}"
-  iam_instance_profile = "${var.iam_instance_profile}"
+	instance_type        = "${var.instance_type}"
+	spot_price           = "${var.spot_price}"
+	iam_instance_profile = "${var.iam_instance_profile}"
 
-  user_data_base64      = "${var.user_data_base64}"
-  key_name              = "${var.key_name}"
+	user_data_base64      = "${var.user_data_base64}"
+	key_name              = "${var.key_name}"
 
-  security_groups       = [ "${var.security_groups}" ]
+	security_groups       = [ "${var.security_groups}" ]
 
-  associate_public_ip_address = "${var.associate_public_ip_address}"
+	associate_public_ip_address = "${var.associate_public_ip_address}"
 
-  root_block_device {
-    volume_type           = "${var.volume_type}"
-    volume_size           = "${var.volume_size}"
-    delete_on_termination = "${var.delete_on_termination}"
-  }
+	root_block_device {
+		volume_type           = "${var.volume_type}"
+ 		volume_size           = "${var.volume_size}"
+ 		delete_on_termination = "${var.delete_on_termination}"
+	}
 
-  ##################################################################
-  # Lifecyclye cannot be parameterized:
-  #  - Cannot use interpolations -
-  ##################################################################
-  lifecycle {
-    ignore_changes        = [ "user_data_base64" ]
-    create_before_destroy = true
-  }
+	##################################################################
+	# Lifecyclye cannot be parameterized:
+	#  - Cannot use interpolations -
+	##################################################################
+	lifecycle {
+		ignore_changes        = [ "user_data_base64" ]
+		create_before_destroy = true
+	}
 
-  ebs_optimized           = "${var.ebs_optimized}"
+	ebs_optimized           = "${var.ebs_optimized}"
 }
