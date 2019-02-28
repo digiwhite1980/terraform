9e022dd2ec6e374f09111c07accd5d6d4fecd74a
Raymond Otto <otto@rgpo.nl>
2019-02-28 13:23:57 +0100

    make policy attach list aware

--- iam_role_policy_attach/iam_role_policy_attach.tf
+++ iam_role_policy_attach/iam_role_policy_attach.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_role_policy_attachment" "policy" {
+	count			= "${length(var.policy_arn)}"
+
 	role 			= "${var.role}"
-	policy_arn 	= "${var.policy_arn}"
+	policy_arn 	= "${element(var.policy_arn, count.index)}"
 }
--- iam_role_policy_attach/variables.tf
+++ iam_role_policy_attach/variables.tf
@@ -1,2 +1,10 @@
+variable "count" 			{
+	default = 1
+}
+
 variable "role"			{}
-variable "policy_arn"	{}
+
+variable "policy_arn"	{
+	type = "list"
+	default = []
+}
